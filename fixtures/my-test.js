8bfbbf0299f1b49a8e22a034e37066758518ad4d
maxteplyakov <56788450+maxteplyakov@users.noreply.github.com>
2022-03-21 14:02:30 +0700

    Remove commented lines

--- fixtures/my-test.js
+++ fixtures/my-test.js
@@ -6,17 +6,14 @@ const { UserModel } = require('../models/usermodel.js');
 exports.test = base.test.extend({
   signupPage: async ({ page }, use) => {
     await use(new SignupPage(page));
-    //await signupPage.removeAll();
   },
 
   accountPage: async ({ page }, use) => {
     await use(new AccountsPage(page));
-    //await accountsPage.removeAll();
   },
 
   user: async ({ page }, use) => {
     await use(new UserModel());
-    //await user.removeAll();
   },
 });
 exports.expect = base.expect;
